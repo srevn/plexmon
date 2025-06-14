fb4f9076453f82b50abe89733d2e6e8a0562d304
my <my@xoxo>
2025-06-14 13:18:46 +0300

    verbose and daemonize as args only

--- src/config.c
+++ src/config.c
@@ -10,30 +10,30 @@ bool config_load(const char *config_path) {
     FILE *fp;
     char line[1024];
     char key[256], value[768];
-    
+
     log_message(LOG_INFO, "Loading configuration from %s", config_path);
-    
+
     fp = fopen(config_path, "r");
     if (!fp) {
-        log_message(LOG_WARNING, "Could not open config file %s: %s", 
+        log_message(LOG_WARNING, "Could not open config file %s: %s",
                     config_path, strerror(errno));
         log_message(LOG_INFO, "Using default configuration");
         return true;  /* Continue with defaults */
     }
-    
+
     /* Parse configuration file */
     while (fgets(line, sizeof(line), fp)) {
         /* Skip comments and empty lines */
         if (line[0] == '#' || line[0] == '\n') {
             continue;
         }
-        
+
         /* Remove trailing newline */
         int len = strlen(line);
         if (len > 0 && line[len-1] == '\n') {
             line[len-1] = '\0';
         }
-        
+
         /* Parse key=value format */
         if (sscanf(line, "%255[^=]=%767s", key, value) == 2) {
             /* Trim whitespace from key */
@@ -41,7 +41,7 @@ bool config_load(const char *config_path) {
             while (isspace(*k)) k++;
             char *end = k + strlen(k) - 1;
             while (end > k && isspace(*end)) *end-- = '\0';
-            
+
             /* Process configuration options */
             if (strcmp(k, "plex_url") == 0) {
                 strncpy(g_config.plex_url, value, PATH_MAX_LEN - 1);
@@ -53,16 +53,6 @@ bool config_load(const char *config_path) {
             }
             else if (strcmp(k, "startup_timeout") == 0) {
                 g_config.startup_timeout = atoi(value);
-            }
-            else if (strcmp(k, "verbose") == 0) {
-                g_config.verbose = (strcmp(value, "true") == 0 || 
-                                    strcmp(value, "yes") == 0 || 
-                                    strcmp(value, "1") == 0);
-            }
-            else if (strcmp(k, "daemonize") == 0) {
-                g_config.daemonize = (strcmp(value, "true") == 0 || 
-                                     strcmp(value, "yes") == 0 || 
-                                     strcmp(value, "1") == 0);
             }
             else if (strcmp(k, "log_level") == 0) {
                 if (strcasecmp(value, "debug") == 0) {
@@ -82,20 +72,20 @@ bool config_load(const char *config_path) {
             }
         }
     }
-    
+
     fclose(fp);
     log_message(LOG_INFO, "Configuration loaded successfully");
-    
+
     /* Validate configuration */
     if (strlen(g_config.plex_token) == 0) {
         log_message(LOG_WARNING, "No Plex token provided in configuration");
     }
-    
+
     if (g_config.startup_timeout <= 0) {
-        log_message(LOG_WARNING, "Invalid startup_timeout (%d), using default of 60s", 
+        log_message(LOG_WARNING, "Invalid startup_timeout (%d), using default of 60s",
                     g_config.startup_timeout);
         g_config.startup_timeout = 60;
     }
-    
+
     return true;
 }
